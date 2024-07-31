6b3fb474c1c854f324549aed8f50b8e971468e1f
OMTAKALE2004 <111902987+OMTAKALE2004@users.noreply.github.com>
2024-08-01 00:04:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,11 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+
+with app.app_context():
+    db.create_all()
+
+
+# if __name__ == '__main__':
+
+    # app.run(debug=True)
